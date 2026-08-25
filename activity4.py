83829c8efeb775ac5fa993f5f91ad79e787e4c07
Neji143 <neillderosas@gmail.com>
2026-08-26 01:36:08 +0800

    Add files via upload

---
+++ activity4.py
@@ -0,0 +1,12 @@
+#escape sequence
+# \n - new line
+# \t - tab
+# \r - carriage return/ deprecated/obsolete
+# \b - backspace
+# \\ - insert slash
+# \" - insert double quotation
+
+phrase1 = "\"Can you unlove someone?\", \\you can't really unlove someone,\nyou just getting used to without having them around, \narayko"
+phrase2= "we are infinite, \ni was here"
+
+print(phrase1)
