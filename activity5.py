c0c23b29ea3093ae91aa9a02dfc9a316c530d828
nejid.rose <neillderosas@gmail.com>
2026-08-26 22:50:28 +0800

    Add files via upload

---
+++ activity5.py
@@ -0,0 +1,20 @@
+#USING OPERATORS
+
+a = 6
+b = 67
+
+sum = a + b
+diff = a - b
+product = a*b
+quotient = a/b
+expo = a**b
+floordiv = a//b
+modulo = a%b
+
+print("The sum of", a, "and", b, "is",sum)
+print("The difference of", a,"and", b, "is", diff)
+print("The product of", a, "and", b, "is", product)
+print("The quotient of", a, "and", b, "is", quotient)
+print("The exponentation of", a, "and", b, "is", expo)
+print("The floor division of", a, "and", b, "is", floordiv)
+print("The modulo of", a, "and", b, "is", modulo)
